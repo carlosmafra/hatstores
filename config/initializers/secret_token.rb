daa35213a4ba259d7b9ba2590e345ebf97f22fd8
Fellipe Guimaraes <freador@gmail.com>
2014-01-15 15:57:40 -0800

    Aplicação Criada

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hatstore::Application.config.secret_key_base = 'f8c9729d7b274952fbb1f98a8e4d7c30cf727435f87cd5a1466521201d38a74715f3027be6b8ca732b84b74af9eeda1de01888a0ad71d363039ba45f1af4d19c'
