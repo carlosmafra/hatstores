17a29bdfd98bbf3392fc8c5641a0b0475ff45bf6
Fellipe Guimaraes <freador@gmail.com>
2014-01-15 16:05:20 -0800

    Configuracao das Bases de Dados e Authenticação

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Hatstore::Application.routes.draw do
+  devise_for :admin_users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
