fc08b87595e8acfd84ea57db0c88b6d8caf2fef4
Fellipe Guimaraes <freador@gmail.com>
2014-01-16 14:33:47 -0800

    Array de Verificação Concluido, Array Verifica se o Código Existe

--- app/controllers/admin/xml_controller.rb
+++ app/controllers/admin/xml_controller.rb
@@ -1,22 +1,23 @@
 require 'open-uri'
 class Admin::XmlController < ApplicationController
 	def index
-
+		@array_produtos = Array.new
+		produtos = Admin::Produto.all
 		doc = Nokogiri::XML(open("http://www.hatstore.com.br/xml/8c1d9/shoppinguol.xml"))
-		@nao_exists = Array.new
-		@exists = Array.new
 		@doc = doc.xpath('//PRODUTOS/PRODUTO').each do |i|
 			#i.xpath('DESCRICAO').children.text
-			xml = Admin::Produto.find_by_code(i.xpath('CODIGO').children.text)
-			if xml
-				@exists << { codigo: i.xpath("CODIGO").children.text, name: i.xpath("DESCRICAO").children.text, url: i.xpath("URL").children.text, preco: i.xpath("PRECO").children.text, nparcela: i.xpath("NPARCELA").children.text, image: i.xpath("URL_IMAGE").children.text, marca: i.xpath("DEPARTAMENTO").children.text , exists: '1'}
-			else
-				@nao_exists << { codigo: i.xpath("CODIGO").children.text, name: i.xpath("DESCRICAO").children.text, url: i.xpath("URL").children.text, preco: i.xpath("PRECO").children.text, nparcela: i.xpath("NPARCELA").children.text, image: i.xpath("URL_IMAGE").children.text, marca: i.xpath("DEPARTAMENTO").children.text, exists: '0' }
+			#xml = Admin::Produto.find_by_code(i.xpath('CODIGO').children.text)
+			produtos.each do |produto|
+				if produto.code.include?(i.xpath('CODIGO').children.text)
+					@array_produtos << { codigo: i.xpath("CODIGO").children.text, name: i.xpath("DESCRICAO").children.text, url: i.xpath("URL").children.text, preco: i.xpath("PRECO").children.text, nparcela: i.xpath("NPARCELA").children.text, image: i.xpath("URL_IMAGE").children.text, marca: i.xpath("DEPARTAMENTO").children.text , exists: '1'}
+				else
+					@array_produtos << { codigo: i.xpath("CODIGO").children.text, name: i.xpath("DESCRICAO").children.text, url: i.xpath("URL").children.text, preco: i.xpath("PRECO").children.text, nparcela: i.xpath("NPARCELA").children.text, image: i.xpath("URL_IMAGE").children.text, marca: i.xpath("DEPARTAMENTO").children.text, exists: '0' }
+				end				
 			end
-			p @nao_exists
 		end
+	end
 
-		p @nao_exists
+	def create_or_update_produtos
 	end
 
 	def read_xml
