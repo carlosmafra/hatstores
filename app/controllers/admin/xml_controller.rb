f99f87a39eafd4797f33034921f5adb176d05a9a
Fellipe Guimaraes <freador@gmail.com>
2014-01-16 13:31:41 -0800

    Array de Verificação Concluido, Array Verifica se o Código Existe

--- app/controllers/admin/xml_controller.rb
+++ app/controllers/admin/xml_controller.rb
@@ -7,13 +7,13 @@ class Admin::XmlController < ApplicationController
 		@exists = Array.new
 		@doc = doc.xpath('//PRODUTOS/PRODUTO').each do |i|
 			#i.xpath('DESCRICAO').children.text
-			p = Produto.find_by_code(i.xpath('CODIGO').children.text)
-			if p
-				p i
-
+			xml = Admin::Produto.find_by_code(i.xpath('CODIGO').children.text)
+			if xml
+				@exists << { codigo: i.xpath("CODIGO").children.text, name: i.xpath("DESCRICAO").children.text, url: i.xpath("URL").children.text, preco: i.xpath("PRECO").children.text, nparcela: i.xpath("NPARCELA").children.text, image: i.xpath("URL_IMAGE").children.text, marca: i.xpath("DEPARTAMENTO").children.text , exists: '1'}
 			else
-				@nao_exists << i				
+				@nao_exists << { codigo: i.xpath("CODIGO").children.text, name: i.xpath("DESCRICAO").children.text, url: i.xpath("URL").children.text, preco: i.xpath("PRECO").children.text, nparcela: i.xpath("NPARCELA").children.text, image: i.xpath("URL_IMAGE").children.text, marca: i.xpath("DEPARTAMENTO").children.text, exists: '0' }
 			end
+			p @nao_exists
 		end
 
 		p @nao_exists
